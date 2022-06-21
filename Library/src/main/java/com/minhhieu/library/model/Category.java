eb39863499477f5ecc9dc6296bb32806b49d2fc1
MinhHieu259 <mhieu7252@gmail.com>
2022-06-21 23:30:38 +0700

    ngu2

--- Library/src/main/java/com/minhhieu/library/model/Category.java
+++ Library/src/main/java/com/minhhieu/library/model/Category.java
@@ -1,2 +1,28 @@
-package com.minhhieu.library.model;public class Category {
+package com.minhhieu.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "categories", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+    private String name;
+    private boolean is_deleted;
+    private boolean is_activeted;
+
+    public Category(String name){
+        this.name = name;
+        this.is_activeted = true;
+        this.is_deleted = false;
+    }
 }
--- Library/src/main/java/com/minhhieu/library/model/Product.java
+++ Library/src/main/java/com/minhhieu/library/model/Product.java
@@ -1,2 +1,34 @@
-package com.minhhieu.library.model;public class Product {
+package com.minhhieu.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products", uniqueConstraints = @UniqueConstraint(columnNames = {"name", "image"}))
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+    private String name;
+    private String description;
+    private double costPrice;
+    private double salePrice;
+    private int currentQuantity;
+    @Lob
+    @Column(columnDefinition = "MEDIUMBLOB")
+    private String image;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+    private boolean is_deleted;
+    private boolean is_activated;
 }
